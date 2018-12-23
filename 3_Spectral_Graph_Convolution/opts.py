8266f593339563823401ad82c9e8bd3b0bae2efe
bumsoo-gpu3 <meliketoy@gmail.com>
2018-12-23 22:49:00 +0900

    Home dir update

--- 3_Spectral_Graph_Convolution/opts.py
+++ 3_Spectral_Graph_Convolution/opts.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import torch
+from pathlib import Path
 
 class BaseOptions():
     def __init__(self):
@@ -8,10 +9,10 @@ class BaseOptions():
         self.initialized = False
 
     def initialize(self):
-        self.parser.add_argument('--dataroot', type=str, default='/home/bumsoo/Data/Planetoid', help='path')
+        self.parser.add_argument('--dataroot', type=str, default=os.path.join(Path.home(), "Data", "Planetoid"), help='path')
         self.parser.add_argument('--dataset', type=str, default='pubmed', help='[cora | citeseer | pubmed]')
-        self.parser.add_argument('--num_hidden', type=int, default=8, help='number of features')
-        self.parser.add_argument('--dropout', type=float, default=0.6, help='dropout')
+        self.parser.add_argument('--num_hidden', type=int, default=32, help='number of features')
+        self.parser.add_argument('--dropout', type=float, default=0.5, help='dropout')
         self.parser.add_argument('--weight_decay', type=float, default=5e-4, help='weight decay')
         self.parser.add_argument('--init_type', type=str, default='uniform', help='[uniform | xavier]')
         self.parser.add_argument('--model', type=str, default='basic', help='[basic]')
@@ -30,11 +31,10 @@ class TrainOptions(BaseOptions):
     # Override
     def initialize(self):
         BaseOptions.initialize(self)
-        self.parser.add_argument('--lr', type=float, default=5e-3, help='initial learning rate')
+        self.parser.add_argument('--lr', type=float, default=1e-3, help='initial learning rate')
         self.parser.add_argument('--optimizer', type=str, default='adam', help='[sgd | adam]')
-        self.parser.add_argument('--epoch', type=int, default=800, help='number of training epochs')
-        self.parser.add_argument('--lr_decay_epoch', type=int, default=5000, help='multiply by a gamma every set iter')
-        self.parser.add_argument('--nb_heads', type=int, default=8, help='number of head attentions')
+        self.parser.add_argument('--epoch', type=int, default=5000, help='number of training epochs')
+        self.parser.add_argument('--lr_decay_epoch', type=int, default=1000, help='multiply by a gamma every set iter')
         self.parser.add_argument('--alpha', type=float, default=0.2, help='Alpha value for the leaky_relu')
         self.isTrain = True
 
